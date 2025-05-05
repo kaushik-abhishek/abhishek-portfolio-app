b044497d9cefb9f9777426547d65737273fe46ff
kaushik-abhishek <vyasdusani@gmail.com>
2025-05-05 23:33:25 +0530

    Add prop-types dependency and update App component structure with BlurBlob and Skills

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import Navbar from "./component/Navbar/Navbar";
 import Footer from "./component/Footer/Footer";
 import About from "./component/About/About";
+import BlurBlob from "./component/BlurBlob";
+import Skills from "./component/Skills/Skills";
 
 const App = () => {
   return (
     <div className="bg-[#050414]">
+      <BlurBlob
+        position={{ top: "35%", left: "20%" }}
+        size={{ width: "30%", height: "40%" }}
+      />
+
       {/* Background Grid */}
       <div
         className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] 
@@ -17,6 +24,7 @@ const App = () => {
       <div className="relative pt-20">
         <Navbar />
         <About />
+        <Skills />
         <Footer />
       </div>
     </div>
